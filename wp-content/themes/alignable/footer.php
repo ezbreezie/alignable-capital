630dc532064f4a4c3390f39b42db6c7d52bd8573
Bria Mears <hertoo@HerToo.local>
2018-07-21 21:29:18 -0400

    content fill-in edits

--- wp-content/themes/alignable/footer.php
+++ wp-content/themes/alignable/footer.php
@@ -9,7 +9,7 @@
 
 <footer>
 	<section class="container">
-		<p>Alignable Capital LLC &#9643; &copy; All Rights Reserved. <span class="visible-xs">&#9643;</span><br class="hidden-xs"> <a href="#">800-000-000</a> &#9643; <a href="#">email@alignablecapital.com</a></p>
+		<p>Alignable Capital LLC &#9643; &copy; All Rights Reserved. <span class="visible-xs">&#9643;</span><br class="hidden-xs"> <a href="tel:1-267-571-5140">267-571-5140</a> &#9643; <a href="mailto:contactus@alignablecapital.com">contactus@alignablecapital.com</a></p>
 	</section>
 </footer>
 
